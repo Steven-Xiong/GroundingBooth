9a96ef2c6319a43a96d100838c0721703e3ab0fd
Steven-Xiong <x.zhexiao@wustl.edu>
2025-07-20 19:47:28 -0500

    init

--- infer.sh
+++ infer.sh
@@ -1,7 +1,14 @@
-python infer_customized_all.py \
-        --batch_size 1 \
-        --guidance_scale 5 \
-        --folder OUTPUT_test/3.8_red_2.20/0.38_0.05_0.82_0.7_$i \
-        --dataset dreambench \
-        --ckpt_path checkpoints/checkpoint_00222001.pth
+# python infer_customized_all.py \
+#         --batch_size 1 \
+#         --guidance_scale 5 \
+#         --folder OUTPUT_test \
+#         --dataset dreambench \
+#         --ckpt_path checkpoints/checkpoint_00222001.pth
 
+python inference_single.py \
+        --batch_size 1 \
+        --guidance_scale 3 \
+        --folder OUTPUT_test \
+        --dataset dreambench \
+        --background \
+        --ckpt_path checkpoints/checkpoint_00222001.pth
